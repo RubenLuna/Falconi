b904c185cbe45dfeada13314009681b6706c0361
RubenLuna <44658142+RubenLuna@users.noreply.github.com>
2018-12-04 23:03:21 -0700

    Add files via upload

---
+++ fx.js
@@ -0,0 +1,79 @@
+﻿
+
+
+
+function fxShowmodal() {
+
+    $('#DonasModal').modal('show');
+
+}
+
+
+function fxReset(){
+
+    $('#txtQuantity').val('');
+}
+
+
+function fxSum() {
+
+    var current = $('#txtQuantity').val();
+
+    var intNew = parseInt(current) + 1;
+
+    $('#txtQuantity').val(intNew);
+
+
+}
+
+
+function fxMinus() {
+
+    var current = $('#txtQuantity').val();
+
+    var intNew = parseInt(current) - 1;
+
+    if (parseInt(intNew) < 0) {
+        $('#txtQuantity').val(0);
+    } else { 
+
+        $('#txtQuantity').val(intNew);
+    }
+
+
+}
+
+$(document).ready(function () {
+
+    $('#txtQuantity').val(0);
+
+
+    $('img').on('click', function () {
+
+        var src = $(this).attr('src');
+        var desc = $(this).next().text();
+        $('#modalImgId').attr('src', src);
+        $('#DonasModalLabel').text('');
+        $('#DonasModalLabel').append('<strong>' + desc + '</strong>');
+        $('#DonasModal').modal('show');
+
+    });
+
+
+    //alert('asd');
+
+
+
+
+
+    //END DOCUMENT READY FUNCTION
+});
+
+
+
+
+
+
+
+
+
